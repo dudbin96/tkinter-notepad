ccc78aefb8e9a9c853b364d2c6d5ce7d55926775
dudbin96 <dudbin96@gmail.com>
2023-11-09 09:36:33 +0900

    Add button event

--- main.py
+++ main.py
@@ -6,18 +6,36 @@ window.title("Notepad")
 window.geometry("400x400")
 window.resizable(FALSE,FALSE)
 
+# 함수 추가(클릭 이벤트)
+def new_file() :
+    text_area.delete(1.0,END)
+
+def save_file() :
+    f = asksaveasfile(mode = "w", defaultextension=".txt", filetypes=[('Text files', '.txt')])
+    text_save = str(text_area.get(1.0,END))
+    f.write(text_save) # text_area의 있는 텍스트를 읽어와 저장한다.
+    f.close()
+
+def maker() :
+    help_view = Toplevel(window)
+    help_view.geometry("300x50")
+    help_view.title("만든이")
+    lb = Label(help_view, text = "전영빈 만든 메모장입니다.")
+    lb.pack()
+
+
 # 메뉴 버튼 추가
 
 menu = Menu(window)
 menu_1 = Menu(menu, tearoff=0)
-menu_1.add_command(label="새파일")
-menu_1.add_command(label="저장")
+menu_1.add_command(label="새파일", command=new_file)
+menu_1.add_command(label="저장", command=save_file)
 menu_1.add_separator()
 menu_1.add_command(label="종료", command= window.destroy)
 menu.add_cascade(label="파일", menu=menu_1)
 
 menu_2 = Menu(menu, tearoff=0)
-menu_2.add_command(label="만든이")
+menu_2.add_command(label="만든이", command=maker)
 menu.add_cascade(label="만든이", menu=menu_2)
 
 # 텍스트 입력 추가
