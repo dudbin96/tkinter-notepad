8e143eb935ceacc8f074204ea2ac82d1c3a838e5
dudbin96 <dudbin96@gmail.com>
2023-11-09 09:30:31 +0900

    Add textarea

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ window.title("Notepad")
 window.geometry("400x400")
 window.resizable(FALSE,FALSE)
 
+# 메뉴 버튼 추가
+
 menu = Menu(window)
 menu_1 = Menu(menu, tearoff=0)
 menu_1.add_command(label="새파일")
@@ -17,6 +19,13 @@ menu.add_cascade(label="파일", menu=menu_1)
 menu_2 = Menu(menu, tearoff=0)
 menu_2.add_command(label="만든이")
 menu.add_cascade(label="만든이", menu=menu_2)
+
+# 텍스트 입력 추가
+text_area = Text(window)
+window.grid_rowconfigure(0, weight=1)
+window.grid_columnconfigure(0, weight=1)
+text_area.grid(sticky= N + E + S + W)
+
 
 window.config(menu=menu)
 
